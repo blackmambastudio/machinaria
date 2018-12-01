a7407d7c71501efca0d3abaca280f1e165cdd7dd
Agar3s <ekeisco@gmail.com>
2018-12-01 06:09:13 -0500

    adds animation transition to news

--- Television/Television.gd
+++ Television/Television.gd
@@ -1,9 +1,24 @@
 extends "res://Interlude/Interlude.gd"
 
 
+func _ready():
+	$presenter.visible = false
+	$Results_Banner.visible = false
+	var intentions = Game_Mode.Election_Simulator.intentions
+	$Results_Banner/Sutano/Votes.text = str(intentions[1]) + "%"
+	$Results_Banner/Fulano/Votes.text = str(intentions[2]) + "%"
+	$Results_Banner/Na/Votes.text = str(intentions[0]) + "%"
+	yield($AnimationPlayer, "animation_finished")
+	$AnimationPlayer.play("scenes")
+
+func display_poll():
+	$presenter.visible = false
+	pass
+
+
 func load_props(props):
-	$anchorman_results_1_logo.visible = true
+	$presenter.visible = true
 
 func close_scene():
-	$anchorman_results_1_logo.visible = false
+	$presenter.visible = false
 	.close_scene()
