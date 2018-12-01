1e8b8091d7fcb789fa1a627e7f1f2022d8fc573c
Agar3s <ekeisco@gmail.com>
2018-12-01 16:20:39 -0500

    fix the fucking Mm problem

--- MiMo/IO/Led_Bar/Led_Bar.gd
+++ MiMo/IO/Led_Bar/Led_Bar.gd
@@ -3,12 +3,12 @@ extends Container
 
 func _ready():
 	var spacing = (rect_size.x - ($Led_01.position.x*2))/8
-	for i in range(1, 8):
-		var led = $Led_01.duplicate()
-		led.name = "Led_0" + str(i + 1)
-		led.position.x += i*spacing
-		add_child(led)
-		led.add_to_group("leds")
+	#for i in range(1, 8):
+	#	var led = $Led_01.duplicate()
+#		led.name = "Led_0" + str(i + 1)
+#		led.position.x += i*spacing
+#		add_child(led)
+#		led.add_to_group("leds")
 
 
 func turn_on_led(index, status):
