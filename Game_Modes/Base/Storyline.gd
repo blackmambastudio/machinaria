8ce39b47f7cfb3ae885962e21d9b379bf58e5223
Agar3s <ekeisco@gmail.com>
2018-12-05 17:08:32 -0500

    adjust timeouts for intermediate scenes

--- Game_Modes/Base/Storyline.gd
+++ Game_Modes/Base/Storyline.gd
@@ -11,47 +11,47 @@ var screens = [{
 	"scene": "res://Interlude/Platanal.tscn",
 	"title": "Platanal\n(Semi-Peripheral Country)",
 	"description": "October 1982",
-	"timeout": 4, 
+	"timeout": 15, 
 	"bg": "default"
 }, {
 	"index": 1,
 	"scene": "res://Interlude/Elections.tscn",
-	"timeout": 12,
+	"timeout": 20,
 	"bg": "default"
 }, {
 	"index": 2,
 	"scene": "res://Interlude/Continent_Politics.tscn",
 	"title": "",
 	"description": "Relationships with Brumas and Telmar have been deteriorating since Platanal decided to adopt the politics from Hunaraga, one of the three world powers, in order to become its first ally in the semi-periphery.",
-	"timeout": 12,
+	"timeout": 20,
 	"bg": "default"
 }, {
 	"index": 3,
 	"scene": "res://Interlude/Interlude.tscn",
 	"title": "",
 	"description": "The M. Corp, located in Hunaraga, has chosen Platanal for testing the second prototype of their latest invention: M.i.M.o.\n\nA machine capable of calculating vote intention in real time when the news are being broadcast.",
-	"timeout": 12,
+	"timeout": 20,
 	"bg": "default"
 }, {
 	"index": 4,
 	"scene": "res://Interlude/Interlude.tscn",
 	"title": "",
 	"description": "You work as an informative director for Opticon, one of the biggest mainstream media broadcast channels in Platanal.",
-	"timeout": 9,
+	"timeout": 20,
 	"bg": "default"
 }, {
 	"index": 5,
 	"scene": "res://Interlude/Interlude.tscn",
 	"title": "Black Mamba",
 	"description": "Presents",
-	"timeout": 5,
+	"timeout": 10,
 	"bg": "default"
 }, {
 	"index": 6,
 	"scene": "res://Interlude/Machinaria_Intro.tscn",
 	"title": "Machinaria",
 	"description": "",
-	"timeout": 10,
+	"timeout": 15,
 	"bg": "default"
 }, {
 	"index": 7,
@@ -59,7 +59,7 @@ var screens = [{
 	"title": "- 10 days until elections -",
 	"description": "The recent earthquake in Telmar has left several deaths and a country in crisis. The presidential candidates have pronounced in this respect.",
 	"no_fade": true,
-	"timeout": 10,
+	"timeout": 20,
 	"bg": "default"
 }, {
 	"index": 8,
@@ -77,7 +77,7 @@ var screens = [{
 	"title": "- 5 days until elections -",
 	"description": "In a statement made by Melano Thompson, Hunaraga's president, it was announced a suspicion in the Brumas presidential elections that took place three months ago.\n\nPresidential candidates have pronounced about it.",
 	"no_fade": true,
-	"timeout": 10,
+	"timeout": 20,
 	"bg": "default"
 }, {
 	"index": 11,
@@ -95,7 +95,7 @@ var screens = [{
 	"title": "- 1 day until elections -",
 	"description": "Melano Thompson, Hunaraga's president, has presented evidence which called into question Telavia Camargo's legitimacy about her election as  Brumas's president.",
 	"no_fade": true,
-	"timeout": 15,
+	"timeout": 20,
 	"bg": "default"
 }, {
 	"index": 14,
@@ -112,21 +112,21 @@ var screens = [{
 	"scene": "res://Interlude/MiMo_Destruction.tscn",
 	"title": "",
 	"description": "November 1982. The M. Corp has announced that the experiment was a successful. The resulting presidential elections in Platanal are evidence of that. After the tests, they destroyed the machine.",
-	"timeout": 15,
+	"timeout": 30,
 	"bg": "default"
 }, {
 	"index": 17,
 	"scene": "res://Interlude/Conclusion.tscn",
 	"title": "June 1983.",
 	"description": "?",
-	"timeout": 20,
+	"timeout": 30,
 	"bg": "default"
 }, {
 	"index": 18,
 	"scene": "res://Interlude/Letter.tscn",
 	"title": "",
 	"description": "$last_text",
-	"timeout": 40,
+	"timeout": 60,
 	"bg": "default"
 }]
 
